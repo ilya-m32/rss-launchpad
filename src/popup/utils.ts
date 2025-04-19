418d7a5367c6d0fe26b98941dad755a7e9f35b7c
Ilya Malyavin <mail@ilya-m.com>
2025-04-19 22:19:06 +0200

    fix: remove redundant tag sanitization (only safe methods are used)

--- src/popup/utils.ts
+++ src/popup/utils.ts
@@ -11,25 +11,12 @@ export function getBrowser(): Browser {
   throw new Error("Unsupported browser");
 }
 
-const escapeMap: { [char: string]: string } = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': "&quot;",
-  "'": "&#39;",
-};
-const deescapeMap = Object.fromEntries(Object.entries(escapeMap).map(([a, b]) => [b, a]));
-
-export function escapeHtml(str: string): string {
-  return str.replace(/[&<>"']/g, (m) => escapeMap[m]);
-}
-
 export function sanitizeFeed(feed: Feed): Feed {
   const type = /application\/(rss|atom)\+\w/.exec(feed.type)?.[1];
 
   return {
     extractType: feed.extractType,
-    title: escapeHtml(feed.title),
+    title: feed.title,
     type: type ?? "UNKWN",
     href: new URL(feed.href).toString(),
   };
@@ -46,10 +33,6 @@ export function sanitizeFeeds(inputFeeds: Feed[]): Feed[] {
     }
   }
   return feeds;
-}
-
-export function deescapeHtml(str: string): string {
-  return str.replace(/&amp;|&lt;|&gt;|&quot;|&#039;|&#x2F;/g, (match) => deescapeMap[match]);
 }
 
 export function getTranslation(tag: string, subs?: string | string[]) {
